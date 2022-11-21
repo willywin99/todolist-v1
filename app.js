a251afa2956deba57743f741a2a8d9d7bb5ca895
willywin99 <willywinsianturi@gmail.com>
2022-11-21 21:28:09 +0700

    Create first EJS Template

--- app.js
+++ app.js
@@ -5,15 +5,41 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
 	let today = new Date();
 	let currentDay = today.getDay();
-	if (currentDay === 6 || currentDay === 0) {
-		res.write("<h1>Yay, it's the weekend</h1>");
-		res.send();
-	} else {
-		res.sendFile(__dirname + '/index.html');
+	let day = "";
+
+	switch (currentDay) {
+		case 0:
+			day = "Sunday";
+			break;
+		case 1:
+			day = "Monday";
+			break;
+		case 2:
+			day = "Tuesday";
+			break;
+		case 3:
+			day = "Wednesday";
+			break;
+		case 4:
+			day = "Thursday";
+			break;
+		case 5:
+			day = "Friday";
+			break;
+		case 6:
+			day = "Saturday";
+			break;
+	
+		default:
+			console.log('Error: current day is equal to: ' + currentDay);
+			break;
 	}
+	res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function() {
