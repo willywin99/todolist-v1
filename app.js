a8f1c87c3ca3151e94df35d8ec370b691edb785e
willywin99 <willywinsianturi@gmail.com>
2022-12-16 17:35:18 +0700

    feat: styles.css Adding Pre-Made CSS Stylesheets to Your Website

--- app.js
+++ app.js
@@ -10,17 +10,18 @@ let items = ['Buy Food', 'Cook Food', 'Eat Food'];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
 	let today = new Date();
 
-	var options = {
+	let options = {
 		weekday: 'long',
 		day: 'numeric',
 		month: 'long'
 	}
 
-	var day = today.toLocaleDateString('en-US', options)
+	let day = today.toLocaleDateString('en-US', options)
 	
 	res.render("list", {kindOfDay: day, newListItems: items});
 });
